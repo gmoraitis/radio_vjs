d24227924c25cafa9a939c5e963a0b15ffff4c33
gmoraitis <moraitis.georgios@icloud.com>
2024-12-26 17:26:17 +0200

    Added back buttons for the controls

--- script.js
+++ script.js
@@ -216,7 +216,6 @@ function showTime() {
     let time = new Date();
     let hour = time.getHours();
     let min = time.getMinutes();
-    let sec = time.getSeconds();
 
     hour = hour < 10 ? "0" + hour : hour;
     min = min < 10 ? "0" + min : min;
