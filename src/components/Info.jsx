e0b6e469b6199a1daf1cff1a4c25e7c5d8ae7d1f
gmoraitis <moraitis.georgios@icloud.com>
2025-01-13 21:57:34 +0200

    Small correction on info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,9 +2,9 @@ function Info() {
   return (
     <div>
       <div id="defaultInfo">
-        <p> A simple vanilla Javascript project to fetch
+        <p> A simple ReactJS project to fetch
           some radio stations from Athens. Its done based on my curiosity and for educational procedures
-          to interact with some Javascript concepts. The API that i used is <a href="https://de1.api.radio-browser.info/#General">Radio Browser.</a>
+          to interact with some React - Javascript concepts. The API that i used is <a href="https://de1.api.radio-browser.info/#General">Radio Browser.</a>
         </p>
       </div>
     </div>
